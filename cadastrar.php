3f720947ebd79f1949ea38c81e194d5cf9050ea9
DANIEL MACEDO ANDRADE DE SOUSA <572125203@alunos.itaqua.local>
2020-02-21 21:56:53 -0300

    criação de arquivo de cadastro para demonstração de métodos http get e post

---
+++ cadastrar.php
@@ -0,0 +1,11 @@
+<meta charset="UTF-8">
+<?php 
+
+$nomeDoUsuario = $_POST["usuario"];
+$numeroDoCpf = $_POST["cpf"];
+$idadeDoUsuario = $_POST["idade"];
+$senhaDoUsuario = $_POST["pass"];
+
+echo "$nomeDoUsuario seu cadastro foi realizado com sucesso"
+
+?>
